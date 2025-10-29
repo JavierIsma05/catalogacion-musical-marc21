4546c257d46f8d824516f6f608e8bb2505f348f1
Javish-07 <javier.aguilar@unl.edu.ec>
2025-10-29 09:43:56 -0500

    Agregando la base, index y nvbar html

--- catalogacion/urls.py
+++ catalogacion/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.formulario_marc21, name='formulario_marc21'),
+    path('', views.index, name='index'),
+    path('plantillas/', views.plantillas, name='plantillas'),
+    path('crear_obra/', views.crear_obra, name='crear_obra'),
 ]
--- catalogacion/views.py
+++ catalogacion/views.py
@@ -1,26 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    return render(request, 'index.html')
 
-'''
-def formulario_marc21(request):
-    return render(request, 'catalogacion/formulario.html')
-'''
+def plantillas(request):
+    return render(request, 'plantillas.html')
 
-from django.shortcuts import render, redirect
-from django.contrib import messages
-from .models import ObraMarc
-from .forms import ObraMarcForm
-
-def formulario_marc21(request):
-    if request.method == 'POST':
-        form = ObraMarcForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            messages.success(request, "✅ Obra MARC guardada correctamente.")
-            return redirect('formulario_marc21')
-        else:
-            messages.error(request, "⚠️ Verifica los campos, hay errores en el formulario.")
-    else:
-        form = ObraMarcForm()
-    return render(request, 'catalogacion/formulario.html', {'form': form})
+def crear_obra(request):
+    return render(request, 'crear_obra.html')
