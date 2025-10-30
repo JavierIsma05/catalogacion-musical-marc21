c8f5cc8a764d5940a6f64552b3cced191358ccd4
Javish-07 <javier.aguilar@unl.edu.ec>
2025-10-30 10:49:29 -0500

    Creando desde 0 los campos General

--- catalogacion/urls.py
+++ catalogacion/urls.py
@@ -5,6 +5,8 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('plantillas/', views.plantillas, name='plantillas'),
     path('crear_obra/', views.crear_obra, name='crear_obra'),
+    # obra general
+    path('obra_general/', views.obra_general, name='obra_general'),
     path('coleccion_manuscrita/', views.coleccion_manuscrita, name='coleccion_manuscrita'),
     path('obra_individual_manuscrita/', views.obra_individual_manuscrita, name='obra_individual_manuscrita'),
     path('coleccion_impresa/', views.coleccion_impresa, name='coleccion_impresa'),
--- catalogacion/views.py
+++ catalogacion/views.py
@@ -9,6 +9,15 @@ def plantillas(request):
 def crear_obra(request):
     return render(request, 'crear_obra.html')
 
+# este obra_general vamos a usar para ir rellenando los campos de la obra general en base al excel 
+# los otros segun veo son herencias del general pero igual hagamos este genral en base al excel 
+# en el mismo excel se visualiza que campos son para manuscrita y cuales para impresa
+# pero a mi modo de verlo todos son campos de la obra general que si bien no se va a visualizar de momento si pongamolo como
+# si fuese el padre de los demas
+def obra_general(request):
+    
+    return render(request, 'ObraGeneral/obra_general.html')
+
 def coleccion_manuscrita(request):
     return render(request, 'ColeccionManuscrita/col_man.html')
 
