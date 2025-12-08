b0bd37ea8dd7a7d357db466941a6c5feae760715
Axlmd <jostin.jimenez@unl.edu.ec>
2025-12-08 11:01:49 -0500

    feat: Actualizar rutas y vistas para la presentación detallada de obras, mejorando la navegación y añadiendo estilos específicos

--- catalogo_publico/urls.py
+++ catalogo_publico/urls.py
@@ -3,6 +3,7 @@ from .views import (
     HomePublicoView,
     ListaObrasPublicaView,
     DetalleObraPublicaView,
+    VistaDetalladaObraView,
 )
 
 app_name = 'catalogo_publico'
@@ -10,5 +11,6 @@ app_name = 'catalogo_publico'
 urlpatterns = [
     path('', HomePublicoView.as_view(), name='home'),
     path('obras/', ListaObrasPublicaView.as_view(), name='lista_obras'),
-    path('obras/<int:pk>/', DetalleObraPublicaView.as_view(), name='detalle_obra'),
+    path('obras/<int:pk>/', DetalleObraPublicaView.as_view(), name='detalle'),
+    path('obras/<int:pk>/detalle/', VistaDetalladaObraView.as_view(), name='detalle_obra'),
 ]
